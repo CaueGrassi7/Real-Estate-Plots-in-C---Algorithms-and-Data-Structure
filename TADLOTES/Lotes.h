b6da47c1f84323c85c1cd74bdb8652e383f6c6ec
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2023-10-16 13:55:33 -0300

    Update Lotes.h

--- TADLOTES/Lotes.h
+++ TADLOTES/Lotes.h
@@ -7,14 +7,13 @@ int tamanho;
 char rua[50];
 int quadra;
 int num;
-
 };
+
 typedef struct Lote lote;
 
 typedef struct No{
-lote a;
+lote valor;
 no* prox;
-
 }no;
 
 typedef struct listaLotes{
@@ -28,6 +27,30 @@ listalotes* crialistalote(){
     l -> inicio = NULL;
     return l;
 }
+int inserelote(listalotes* l, lote a){
+    if(l == NULL) return -1;
+    if(l->inicio ==  NULL) return 1;
+    no* cl = (no*) malloc(sizeof(no));
+    no* aux;
+    aux = l->inicio;
+    while(aux->prox != NULL && strcmp(a.rua, (aux->prox->valor).rua)>0){
+        aux = aux->prox;
+    }
+    cl->prox = aux->prox;
+    aux->prox = cl;
+    cl->valor = a;
+    return 0;
+}
+int buscalote(listalotes* l, lote a);
+int statuslote(listalotes* l, int pos);
+int mudastatus(listalotes* l, int pos);
+int removelote(listalotes* l, int pos);
+void mostrarlotes(listalotes* l);
+
+
+
+
+
 
 
 #endif
