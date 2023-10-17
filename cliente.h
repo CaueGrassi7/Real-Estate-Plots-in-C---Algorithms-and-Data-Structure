97ec29e6370dae1c2adda3fa970e58a1c6581cdd
Murilo Alves Beppler <murilobeppler@hotmail.com>
2023-10-16 22:43:27 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,6 +1,27 @@
-#ifndef ENCADUPLA_H_INCLUDED
-#define ENCADUPLA_H_INCLUDED
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
+
+typedef struct Corretor corretor;
+
+typedef struct NO no;
+
+typedef struct Lista lista;
+
+lista* criar ();
+
+void login ();
+
+void visualizarClientes ();
+
+int listaVazia (lista* l);
+
+int ordenarAlfabetico (lista* l);
 
 
+int tamanho (lista* l);
 
-#endif // ENCADUPLA_H_INCLUDED
+int novoCadastro ();
+
+int excluirCliente ();
+
+#endif 
