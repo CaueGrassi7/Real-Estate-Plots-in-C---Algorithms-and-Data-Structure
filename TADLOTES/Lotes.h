dbe91343c795f7b6b180572ac2a9d65f5d7bb369
Enzo Faria <enzo.lacerda@ufu.br>
2023-10-19 09:45:48 -0300

    TAD Lotes Atualizado

--- TADLOTES/Lotes.h
+++ TADLOTES/Lotes.h
@@ -3,63 +3,47 @@
 
 struct Lote{
 
+int id;
 int tamanho;
 char rua[50];
 int quadra;
 int num;
 int status;
+char* cliente;
+char* creci;
 };
 
 typedef struct Lote lote;
 
 typedef struct No{
 lote valor;
-int pos;
 no* prox;
 }no;
 
 typedef struct listaLotes{
 no* inicio;
+int cod;
 
 }listalotes;
 
-listalotes* crialistalote(){
-    listalotes* l = (listalotes*) malloc (sizeof(listalotes));
-    l -> inicio = NULL;
-    return l;
-}
-
-/*
-int inserelote(listalotes* l, lote a){
-    if(l == NULL) return -1;
-    if(l->inicio ==  NULL) return 1;
-    no* cl = (no*) malloc(sizeof(no));
-    no* aux;
-    aux = l->inicio;
-    while(aux->prox != NULL && strcmp(a.rua, (aux->prox->valor).rua)>0){
-        aux = aux->prox;
-    }
-    cl->prox = aux->prox;
-    aux->prox = cl;
-    cl->valor = a;
-    return 0;
-}
-*/
+listalotes* crialistalote();
 
 
 
+int inserelote(listalotes* l, lote a);
 int buscalote(listalotes* l, lote a);
-int statuslote(listalotes* l, int pos);
-int mudastatus(listalotes* l, int pos);
-int removelote(listalotes* l, int pos);
+int statuslote(listalotes* l, int id);
+int mudastatus(listalotes* l, int id, int status);
+int removelote(listalotes* l, int id);
 void mostrarlotes(listalotes* l);
 void mostrardisponivel(listalotes* l);
 void mostrarreservados(listalotes* l);
 void mostrarvendidos(listalotes* l);
-int mostrarua(listalotes* l, char* rua);
-int mostrapos(listalotes* l, int pos);
-int mostracorretor(listalotes* l, char* creci);
-int mostracliente(listalotes* l, cliente a);
+void mostrarua(listalotes* l, char* rua);
+void mostraid(listalotes* l, int id);
+void mostracorretor(listalotes* l, char* creci);
+void mostracliente(listalotes* l, cliente a);
+int lotevendido(listalotes* l, int id, cliente a, int creci);
 
 
 
