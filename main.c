1e4c1ac64b1955a4553f0692fcf20aa950e019e5
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2023-10-23 14:09:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "Lotes.h"
+#include "clientela.h"
+#include "cliente.h"
 
 int main() {
 
-    printf("Hello World!\n");
-
+    
+    int opcao;
+    while(1){
+    char email[40], senha[40];
+    printf("Seja bem vindo!\n\n1- Login\n2-Cadastro\n3- Sobre\n4- Sair\n");
+    if(opcao == 1){
+        system(cls);
+        printf("Seja bem vindo novamente!\n\nInsira seu email:\n");
+        scanf("%s", email);
+        printf("Ótimo! Agora, sua senha:\n");
+        scanf("%s", senha);
+    //   VerificaUsuario(l, usuario, senha);
+    }
+    else if(opcao == 2){
+        system(cls);
+        printf("Seja bem vindo!\n\nInsira seu email:\n");
+        scanf("%s", email);
+        printf("Ótimo! Agora, a senha que deseja utilizar:\n");
+        scanf("%s", senha);
+    //  InserirUsuario(l, usuario, senha);
+    } else if(opcao == 3){
+        
+    }
+    else if(opcao == 4)break;
+    
+    }
     return 0;
 }
